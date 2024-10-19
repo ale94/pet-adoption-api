681f91281f279830b20841972958d2feee852a29
ale94 <alejandrorua111@gmail.com>
2024-10-19 18:30:20 -0300

    feat: pet-type repositorio creado.

--- src/main/java/ar/com/ale94/pet_adoption_api/repositories/UserRepository.java
+++ src/main/java/ar/com/ale94/pet_adoption_api/repositories/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<UserRepository, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByName(String name);
 }
