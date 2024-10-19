0be6fc15c6c62ad632b3f261f97099ed755ddc57
ale94 <alejandrorua111@gmail.com>
2024-10-19 12:51:12 -0300

    feat: relacion pet y pet-type creada.

--- src/main/java/ar/com/ale94/pet_adoption_api/entities/PetTypeEntity.java
+++ src/main/java/ar/com/ale94/pet_adoption_api/entities/PetTypeEntity.java
@@ -1,12 +1,11 @@
 package ar.com.ale94.pet_adoption_api.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Set;
 
 @Entity(name = "pet-type")
 @Data
@@ -20,4 +19,14 @@ public class PetTypeEntity implements Serializable {
     private Long id;
     @Column(nullable = false)
     private String name;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @OneToMany(
+        cascade = CascadeType.ALL,
+        fetch = FetchType.EAGER,
+        orphanRemoval = true,
+        mappedBy = "petType"
+    )
+    private Set<PetEntity> pets;
 }
