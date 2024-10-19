806cbd8341d080dde6f487559f6ef6f62042d99f
ale94 <alejandrorua111@gmail.com>
2024-10-19 12:07:02 -0300

    feat: entidad user creada.

--- src/main/java/ar/com/ale94/pet_adoption_api/entities/UserEntity.java
+++ src/main/java/ar/com/ale94/pet_adoption_api/entities/UserEntity.java
@@ -1,4 +1,32 @@
 package ar.com.ale94.pet_adoption_api.entities;
 
-public class UserEntity {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity(name = "user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+    private String phone;
+    @Column(nullable = false)
+    private String address;
+    @Column(nullable = false)
+    private LocalDateTime registrationDate;
+
 }
